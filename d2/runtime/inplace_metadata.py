bug fix. no hang but numerically incorrect

d2/runtime/inplace_metadata.py
@@ -214,20 +214,25 @@ def compute_attn_layout_seqlens(
     # if dispatch[i, j] = k, then local_indices_flat[i, j, k] = l means sequence [i,j] is at out_sequence [k,l]
     # out_seqlens_q[k, l] = seq_shard_len[i, j]
     max_num_seq = int(local_indices_flat.max().item() + 1)
+    scatter_index = (flatten_dispatch * (flatten_dispatch >= 0)) * max_num_seq + local_indices_flat
+
+    src_seqlens = seq_shard_len.flatten()
+    src_seq_lens_kv = seq_shard_cumsum.flatten()
+
     out_seqlens_q = torch.zeros(world_size * max_num_seq, dtype=torch.int64, device=dispatch.device)
     out_seqlens_kv = torch.zeros(world_size * max_num_seq, dtype=torch.int64, device=dispatch.device)
-
-    scatter_index = (flatten_dispatch * (flatten_dispatch >= 0)) * max_num_seq + local_indices_flat
-    src_seqlens = seq_shard_len.flatten()
     out_seqlens_q.scatter_(0, scatter_index, src_seqlens)
-    out_seqlens_kv.scatter_(0, scatter_index, src_seqlens)
+    out_seqlens_kv.scatter_(0, scatter_index, src_seq_lens_kv)
     out_seqlens_q = out_seqlens_q.reshape(world_size, max_num_local_seqs)
     out_seqlens_kv = out_seqlens_kv.reshape(world_size, max_num_local_seqs)
+
     num_local_seqs_recv = local_indices_flat.reshape(-1, world_size).max(dim=0)[0] + 1
+
     cu_seqlens_q = out_seqlens_q.cumsum(dim=1)
     cu_seqlens_kv = out_seqlens_kv.cumsum(dim=1)
     max_seqlen_q = out_seqlens_q.max(dim=1)[0]
     max_seqlen_kv = out_seqlens_kv.max(dim=1)[0]
+
     return cu_seqlens_q, cu_seqlens_kv, max_seqlen_q, max_seqlen_kv, num_local_seqs_recv
 
 

d2/runtime/megatron_patch/dispatcher_wrapper.py
@@ -38,9 +38,9 @@ class n_to_n_dispatch(torch.autograd.Function):
     def forward(
         ctx, query_in: torch.Tensor,
         query_metadata: Metadata,
+        rev_query_metadata: Metadata,
         key_value_in: Optional[torch.Tensor]=None,
         key_value_metadata: Optional[Metadata]=None,
-        rev_query_metadata: Optional[Metadata]=None,
         rev_key_value_metadata: Optional[Metadata]=None,
         stream: Optional[torch.cuda.Stream]=None,
         event: Optional[torch.cuda.Event]=None,

d2/runtime/megatron_patch/transformer_layer.py
@@ -75,9 +75,9 @@ class TransformerLayer(MegatronTransformerLayer):
         attn_out_mlp_layout, _ = n_to_n_dispatch.apply(
             attn_out, # query_in
             packed_seq_params.attn_to_mlp_metadata, # query_metadata
+            packed_seq_params.mlp_to_attn_metadata, # rev_query_metadata
             None, # key_value_in
             None, # key_value_metadata
-            packed_seq_params.mlp_to_attn_metadata, # rev_query_metadata
             None, # rev_key_value_metadata
             packed_seq_params.stream, # stream
             self.comm_event, # event
@@ -88,9 +88,9 @@ class TransformerLayer(MegatronTransformerLayer):
         return attn_out_mlp_layout
 
     def _layout_mlp_to_attn(
-            self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor,
-            packed_seq_params: PingPangPackedSeqParams
-        ):
+        self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor,
+        packed_seq_params: PingPangPackedSeqParams
+    ):
         # FIXME: current we do a walk around: merge head dimension into hidden dimension
         #### NOTE: this is a hack. We should fix this in the future.
         assert query.dim() == 3, f"{query.shape=}, should be tnh layout"
@@ -103,13 +103,13 @@ class TransformerLayer(MegatronTransformerLayer):
         value = value.reshape(value.shape[0], num_kv_heads * kv_head_dim)
         ####
 
-        key_value = torch.cat([key, value], dim=1)
+        key_value = torch.cat([key, value], dim=-1).contiguous()
         query_attn_layout, key_value_attn_layout = n_to_n_dispatch.apply(
             query,  # query_in
             packed_seq_params.mlp_to_attn_metadata, # query_metadata
+            packed_seq_params.attn_to_mlp_metadata, # rev_query_metadata
             key_value, # key_value_in
             packed_seq_params.mlp_to_attn_kv_metadata, # key_value_metadata
-            packed_seq_params.attn_to_mlp_metadata, # rev_query_metadata
             packed_seq_params.mlp_to_attn_kv_grad_metadata, # rev_key_value_metadata
             packed_seq_params.stream, # stream
             self.comm_event, # event
@@ -174,7 +174,7 @@ class TransformerLayer(MegatronTransformerLayer):
         # 2. pre-self-attention forward microbatch 0.
         # Ideally, this part should merge to the previous layer's post-self-attention to maximize
         # the communication-computation overlap.
-        query_0, key_0, value_0, rotary_pos_emb_0, residual_0, attn_mask_type_0 = self._forward_pre_core_attn(
+        query_0, key_0, value_0, residual_0, attn_mask_type_0 = self._forward_pre_core_attn(
             hidden_states_0,
             attention_mask_0,
             context_0,
@@ -189,7 +189,7 @@ class TransformerLayer(MegatronTransformerLayer):
         # 3. pre-attention forward of microbatch 1, mlp2attn all2all of microbatch 0
         self.comm_event.wait(torch.cuda.current_stream())
         query_0, key_0, value_0 = self._layout_mlp_to_attn(query_0, key_0, value_0, packed_seq_params_0)
-        query_1, key_1, value_1, rotary_pos_emb_1, residual_1, attn_mask_type_1 = self._forward_pre_core_attn(
+        query_1, key_1, value_1, residual_1, attn_mask_type_1 = self._forward_pre_core_attn(
             hidden_states_1,
             attention_mask_1,
             context_1,
@@ -337,7 +337,7 @@ class TransformerLayer(MegatronTransformerLayer):
             # absolute positional embedding.
             # otherwise, only relative positional embedding takes effect
             # value_layer = apply_rotary_pos_emb(value_layer, k_pos_emb)
-        return query, key, value, rotary_pos_emb, residual, attn_mask_type
+        return query, key, value, residual, attn_mask_type
 
     def _forward_core_attn(
         self,
@@ -479,7 +479,7 @@ class TransformerLayer(MegatronTransformerLayer):
 
         setattr(packed_seq_params, "stream", torch.cuda.current_stream())
 
-        query, key, value, rotary_pos_emb, residual, attn_mask_type = self._forward_pre_core_attn(
+        query, key, value, residual, attn_mask_type = self._forward_pre_core_attn(
             hidden_states,
             rotary_pos_emb,
             rotary_pos_cos,

tests/test_megatron_layer.py
@@ -109,7 +109,7 @@ class MegatronBaseWorker:
         os.environ["MASTER_ADDR"] = master_addr
         os.environ["MASTER_PORT"] = master_port
 
-    def init_comm(self, stride: int, max_tokens_query: int, max_tokens_key_value: int,
+    def init_comm(self, stride_q: int, stride_kv: int, max_tokens_query: int, max_tokens_key_value: int,
                   parallel_config: ParallelConfig):
         # Init megatron communication.
         if not torch.distributed.is_initialized():
@@ -137,8 +137,8 @@ class MegatronBaseWorker:
         torch.distributed.broadcast(uid, src=0)
 
         DispatcherWrapper.init(
-            q_stride=stride,
-            kv_stride=stride,
+            q_stride=stride_q,
+            kv_stride=stride_kv,
             max_tokens_query=max_tokens_query,
             max_tokens_key_value=max_tokens_key_value,
             rank=self.rank,
@@ -169,7 +169,10 @@ class MegatronLayerWorker(MegatronBaseWorker):
         )
         tensor_input = tensor_input.cuda()
         self.layer.train()
-        return self.layer(tensor_input, packed_seq_params=packed_seq_params)
+        out = self.layer(tensor_input, packed_seq_params=packed_seq_params)
+        torch.cuda.synchronize()
+        print(self.rank, "normal forward done")
+        return out
 
     def forward_ping_pang(self, tensor_input: torch.Tensor, packed_seq_params: PingPangPackedSeqParams):
         packed_seq_params = packed_seq_params.to_device()
@@ -181,7 +184,10 @@ class MegatronLayerWorker(MegatronBaseWorker):
         packed_seq_params = packed_seq_params.to_device()
         tensor_input = tensor_input.cuda()
         self.layer.train()
-        return self.layer.forward_one_stage(tensor_input, packed_seq_params=packed_seq_params)
+        out = self.layer.forward_one_stage(tensor_input, packed_seq_params=packed_seq_params)
+        torch.cuda.synchronize()
+        print(self.rank, "ping-pong one stage forward done")
+        return out
 
 def get_seqlen_shard(cu_seqlens_q: torch.Tensor, cu_seqlens_kv: torch.Tensor,
                      max_seqlen_q: torch.Tensor, max_seqlen_kv: torch.Tensor, num_local_seqs_recv: torch.Tensor, rank: int):
@@ -309,12 +315,12 @@ def test_dp_single_split(workers, seed: int, num_tokens: int, max_cp_degree: int
     (cu_seqlens_q_pp, cu_seqlens_kv_pp, max_seqlen_q_pp, max_seqlen_kv_pp, num_local_seqs_recv_pp) = compute_attn_layout_seqlens(
         sp_seq_lens, sp_dst_kv_len, sp_query_dst
     )
-    print(f"{seq_lens=}, {sp_seq_lens=}, {sp_query_dst=}, {sp_dst_kv_len=}")
 
     # Create tensor input
     tensor_input = torch.randn(world_size, num_tokens, hidden_size, dtype=torch.float16)
     ref_ans_handles = []
     ans_handles = []
+    args = []
     for rank in range(world_size):
         tensor_input_local = tensor_input[rank].unsqueeze(1)
         fwd_q_metadata_local = fwd_q_metadata.get_slice(rank)
@@ -351,19 +357,20 @@ def test_dp_single_split(workers, seed: int, num_tokens: int, max_cp_degree: int
             mlp_to_attn_kv_metadata=fwd_kv_metadata_local,
             mlp_to_attn_kv_grad_metadata=rev_kv_metadata_local,
         )
-        print(f"{rank=}, {packed_seq_params_normal=}, {packed_seq_params_stage_0=}")
 
         # Compute the reference answer and test result
         ref_ans_handle = workers[rank].forward_normal.remote(
             tensor_input_local, packed_seq_params_normal
         )
-
-        ans_handle = workers[rank].forward_ping_pang_one_stage.remote(
-            tensor_input_local, packed_seq_params_stage_0
-        )
+        args.append([tensor_input_local, packed_seq_params_stage_0])
         ref_ans_handles.append(ref_ans_handle)
-        ans_handles.append(ans_handle)
     ref_ans = ray.get(ref_ans_handles)
+
+    for rank in range(world_size):
+        ans_handle = workers[rank].forward_ping_pang_one_stage.remote(
+            *args[rank]
+        )
+        ans_handles.append(ans_handle)
     ans = ray.get(ans_handles)
     for r, a in zip(ref_ans, ans):
         torch.testing.assert_close(r, a)
@@ -373,7 +380,8 @@ def init_test(args):
     ray.init()
     workers = create_pg(args.num_nodes, args.num_gpus_per_node, MegatronLayerWorker)
     print("Workers created")
-    stride = args.hidden_size * torch.float16.itemsize
+    stride_q = args.hidden_size * torch.float16.itemsize
+    stride_kv = args.hidden_size * torch.float16.itemsize * 2
     world_size = len(workers)
     max_tokens_query = args.num_tokens * world_size
     max_tokens_key_value = args.num_tokens * world_size
@@ -381,7 +389,7 @@ def init_test(args):
         tensor_model_parallel_size=args.tp_size
     )
     ray.get([worker.init_comm.remote(
-        stride, max_tokens_query, max_tokens_key_value, parallel_config
+        stride_q, stride_kv, max_tokens_query, max_tokens_key_value, parallel_config
     ) for worker in workers])
     print("Communication groups initialized")
 