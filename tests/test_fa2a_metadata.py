e9e6f0d0ab5fa5905664fe4f87f334f0f27d019c
ZYHowell <yhzhuang@cmu.edu>
2025-08-01 01:54:04 +0000

    pass reverse qkv metadata

--- tests/test_fa2a_metadata.py
+++ tests/test_fa2a_metadata.py
@@ -35,7 +35,7 @@ def simulate_fa2a_send_copy_cp(
     k: Tensor, dst_tensor: Tensor,
     k_offsets: Tensor, k_seqlen: Tensor,
     hidden_size_k: int, element_size: int, max_cp: int,
-    send_dst: Tensor
+    send_mask: Tensor
 ):
     """
     Simulate the fa2a send: MLP layout (CP) -> a2a layout.
@@ -46,10 +46,10 @@ def simulate_fa2a_send_copy_cp(
         k_seq_offset = 0
         for seq_id, seq_len in enumerate(k_seqlen):
             k_size = seq_len * hidden_size_k
-            if send_dst[seq_id, cp_id] >= 0:
+            if not send_mask[seq_id, cp_id]:
                 k_seq_offset += k_size
                 continue
-            k_offset_bytes = k_offsets[seq_id, cp_id]
+            k_offset_bytes = k_offsets[cp_id, seq_id]
             k_dst_offset = k_offset_bytes // element_size
             dst_tensor[k_dst_offset:k_dst_offset + k_size] = (
                 k[k_seq_offset:k_seq_offset + k_size]
@@ -79,18 +79,17 @@ def simulate_fa2a_recv_copy_kv_grad(
     k: Tensor, src_tensor: Tensor,
     k_offset: Tensor, k_seqlen: Tensor,
     hidden_size_k: int, element_size: int, max_cp: int,
-    send_dst: Tensor
+    send_mask: Tensor
 ):
     """
     Simulate the fa2a recv: a2a layout -> MLP grad CP layout.
     MLP grad CP layout is in form of: (max_cp, seq_len)
     """
-    k_offset = 0
     for cp_id in range(max_cp):
         k_seq_offset = 0
         for seq_id, seq_len in enumerate(k_seqlen):
             k_size = seq_len * hidden_size_k
-            if send_dst[cp_id, seq_id] >= 0:
+            if not send_mask[seq_id, cp_id]:
                 k_seq_offset += k_size
                 continue
             k_offset_bytes = k_offset[cp_id, seq_id]
@@ -107,18 +106,38 @@ def simulate_fa2a_send_qkv_copy(
     dst_tensor: Tensor, q_offset: Tensor, k_offset: Tensor, v_offset: Tensor,
     q_seqlen: Tensor, k_seqlen: Tensor,
     hidden_size_q: int, hidden_size_k: int, element_size: int, max_cp: int,
-    kv_send_dst: Tensor,
+    send_mask: Tensor,
 ):
+    """QKV (MLP) -> a2a send layout"""
     dst_tensor = simulate_fa2a_send_copy_non_cp(
         q, dst_tensor, q_offset, q_seqlen, hidden_size_q, element_size
     )
     dst_tensor = simulate_fa2a_send_copy_cp(
         k, dst_tensor, k_offset, k_seqlen, hidden_size_k, element_size,
-        max_cp, kv_send_dst
+        max_cp, send_mask
     )
     dst_tensor = simulate_fa2a_send_copy_cp(
         v, dst_tensor, v_offset, k_seqlen, hidden_size_k, element_size,
-        max_cp, kv_send_dst
+        max_cp, send_mask
+    )
+    return dst_tensor
+
+
+def simulate_fa2a_send_qkv_copy_rev(
+    q: Tensor, k: Tensor, v: Tensor,
+    dst_tensor: Tensor, q_offset: Tensor, k_offset: Tensor, v_offset: Tensor,
+    q_seqlen: Tensor, k_seqlen: Tensor,
+    hidden_size_q: int, hidden_size_k: int, element_size: int
+):
+    """QKV grad (ATTN) -> a2a send layout"""
+    dst_tensor = simulate_fa2a_send_copy_non_cp(
+        q, dst_tensor, q_offset, q_seqlen, hidden_size_q, element_size
+    )
+    dst_tensor = simulate_fa2a_send_copy_non_cp(
+        k, dst_tensor, k_offset, k_seqlen, hidden_size_k, element_size,
+    )
+    dst_tensor = simulate_fa2a_send_copy_non_cp(
+        v, dst_tensor, v_offset, k_seqlen, hidden_size_k, element_size,
     )
     return dst_tensor
 
@@ -129,6 +148,7 @@ def simulate_fa2a_recv_copy_qkv(
     q_seqlen: Tensor, k_seqlen: Tensor,
     hidden_size_q: int, hidden_size_k: int, element_size: int
 ):
+    """a2a recv layout -> QKV (ATTN)"""
     q = simulate_fa2a_recv_copy_non_cp(
         q, src_tensor, q_offset, q_seqlen, hidden_size_q, element_size
     )
@@ -137,6 +157,28 @@ def simulate_fa2a_recv_copy_qkv(
     )
     v = simulate_fa2a_recv_copy_non_cp(
         v, src_tensor, v_offset, k_seqlen, hidden_size_k, element_size
+    )
+    return q, k, v
+
+
+def simulate_fa2a_recv_copy_qkv_rev(
+    q: Tensor, k: Tensor, v: Tensor,
+    src_tensor: Tensor, q_offset: Tensor, k_offset: Tensor, v_offset: Tensor,
+    q_seqlen: Tensor, k_seqlen: Tensor,
+    hidden_size_q: int, hidden_size_k: int, element_size: int,
+    max_cp: int, send_mask: Tensor
+):
+    """a2a recv layout -> QKV grad (MLP)"""
+    q = simulate_fa2a_recv_copy_non_cp(
+        q, src_tensor, q_offset, q_seqlen, hidden_size_q, element_size
+    )
+    k = simulate_fa2a_recv_copy_kv_grad(
+        k, src_tensor, k_offset, k_seqlen, hidden_size_k, element_size,
+        max_cp, send_mask,
+    )
+    v = simulate_fa2a_recv_copy_kv_grad(
+        v, src_tensor, v_offset, k_seqlen, hidden_size_k, element_size,
+        max_cp, send_mask
     )
     return q, k, v
 
@@ -158,12 +200,19 @@ def simulate_fa2a(send_buffer: Tensor, recv_buffer: Tensor,
     return recv_buffer
 
 
-def simulate_qkv_a2a_fwd(
+def get_seq_len_slice(metadata: Metadata, rank):
+    return metadata.seq_len[rank][:metadata.num_seqs[rank]]
+
+
+def simulate_qkv_a2a(
     q: Tensor, k: Tensor, v: Tensor,
     metadata: FastAlltoAllMetadata,
-    q_metadata: Metadata, kv_metadata: Metadata,
-    rev_seqlen_q: Tensor, rev_seqlen_kv: Tensor,
-    element_size: int, hidden_q: int, hidden_k: int
+    # Only to get the seq len and max num recv tokens
+    fwd_q_metadata: Metadata, fwd_kv_metadata: Metadata,
+    rev_q_metadata: Metadata, rev_kv_metadata: Metadata,
+    element_size: int, hidden_q: int, hidden_k: int,
+    max_cp: int, kv_comm_mask: Tensor,
+    is_fwd: bool
 ):
     world_size = q.shape[0]
     assert k.shape[0] == world_size
@@ -185,52 +234,67 @@ def simulate_qkv_a2a_fwd(
     )
     for rank in range(world_size):
         metadata_slice = metadata.get_slice(rank)
-        q_metadata_slice = q_metadata.get_slice(rank)
-        kv_metadata_slice = kv_metadata.get_slice(rank)
         q_src_offsets, k_src_offsets, v_src_offsets = metadata_slice.send_memcpy_metadata
-        q_src_seqlen = q_metadata_slice.seq_len
-        kv_src_seqlen = kv_metadata_slice.seq_len
-        max_cp = kv_metadata_slice.dst_rank.shape[1]
+        q_src_seqlen = get_seq_len_slice(fwd_q_metadata, rank)
+        kv_src_seqlen = get_seq_len_slice(fwd_kv_metadata, rank)
 
-        src_buffer[rank] = simulate_fa2a_send_qkv_copy(
+        args = (
             q[rank], k[rank], v[rank], src_buffer[rank],
             q_src_offsets, k_src_offsets, v_src_offsets,
             q_src_seqlen, kv_src_seqlen,
-            hidden_q, hidden_k, element_size, max_cp,
-            kv_metadata_slice.dst_rank
+            hidden_q, hidden_k, element_size,
         )
+        if is_fwd:
+            args += (max_cp, kv_comm_mask[rank])
+            copy_fn = simulate_fa2a_send_qkv_copy
+        else:
+            copy_fn = simulate_fa2a_send_qkv_copy_rev
+        src_buffer[rank] = copy_fn(*args)
 
     dst_buffer = simulate_fa2a(
         src_buffer, dst_buffer, metadata.fa2a_metadata, element_size
     )
 
-    num_recv_tokens_q = q_metadata.num_recv_tokens[:, -1]
+    num_recv_tokens_q = fwd_q_metadata.num_recv_tokens[:, -1]
     max_num_recv_tokens_q = int(torch.max(num_recv_tokens_q).item())
-    num_recv_tokens_k = kv_metadata.num_recv_tokens[:, -1]
+    num_recv_tokens_k = fwd_kv_metadata.num_recv_tokens[:, -1]
     max_num_recv_tokens_k = int(torch.max(num_recv_tokens_k).item())
     dst_q = torch.zeros(
         (world_size, max_num_recv_tokens_q * hidden_q),
         device=q.device, dtype=q.dtype
     )
-    dst_k = torch.zeros(
-        (world_size, max_num_recv_tokens_k * hidden_k),
-        device=k.device, dtype=k.dtype
-    )
-    dst_v = dst_k.clone()
+    if is_fwd:
+        dst_k = torch.zeros(
+            (world_size, max_num_recv_tokens_k * hidden_k),
+            device=k.device, dtype=k.dtype
+        )
+        dst_v = dst_k.clone()
+    else:
+        dst_k = torch.zeros(
+            (world_size, max_cp, max_num_recv_tokens_q * hidden_k),
+            device=k.device, dtype=k.dtype
+        )
+        dst_v = dst_k.clone()
     for rank in range(world_size):
         metadata_slice = metadata.get_slice(rank)
-        q_metadata_slice = q_metadata.get_slice(rank)
-        kv_metadata_slice = kv_metadata.get_slice(rank)
         q_recv_offsets, k_recv_offsets, v_recv_offsets = metadata_slice.recv_memcpy_metadata
-        assert all(t.ndim == 1 for t in (q_recv_offsets, k_recv_offsets, v_recv_offsets))
-        max_cp = kv_metadata_slice.dst_rank.shape[1]
+        rev_seqlen_q = get_seq_len_slice(rev_q_metadata, rank)
+        rev_seqlen_kv = get_seq_len_slice(rev_kv_metadata, rank)
 
-        q_slice, k_slice, v_slice = simulate_fa2a_recv_copy_qkv(
+        args = (
             dst_q[rank], dst_k[rank], dst_v[rank],
-            dst_buffer[rank], q_recv_offsets, k_recv_offsets, v_recv_offsets,
-            rev_seqlen_q[rank], rev_seqlen_kv[rank],
-            hidden_q, hidden_k, element_size
+            dst_buffer[rank], q_recv_offsets, 
+            k_recv_offsets, v_recv_offsets,
+            rev_seqlen_q, rev_seqlen_kv,
+            hidden_q, hidden_k, element_size,
         )
+        if not is_fwd:
+            args += (max_cp, kv_comm_mask[rank])
+            copy_fn = simulate_fa2a_recv_copy_qkv_rev
+        else:
+            copy_fn = simulate_fa2a_recv_copy_qkv
+
+        q_slice, k_slice, v_slice = copy_fn(*args)
         dst_q[rank] = q_slice
         dst_k[rank] = k_slice
         dst_v[rank] = v_slice
@@ -271,6 +335,19 @@ def test(args):
     output_tensor_k = orchestrate_simulate(tensor_k, output_tensor_k, fwd_k_metadata)
     output_tensor_v = orchestrate_simulate(tensor_v, output_tensor_v, fwd_k_metadata)
     print("correct answer done.")
+    rev_tensor_q = torch.zeros((world_size, total_seq_len, hidden_size_q),
+                             device=device, dtype=output_tensor_q.dtype)
+    rev_tensor_k = torch.zeros((world_size, total_seq_len * max_cp_degree, hidden_size_k), device=device)
+    rev_tensor_v = torch.zeros((world_size, total_seq_len * max_cp_degree, hidden_size_k), device=device)
+
+    rev_tensor_q = orchestrate_simulate(output_tensor_q, rev_tensor_q, rev_q_metadata)
+    rev_tensor_k = orchestrate_simulate(output_tensor_k, rev_tensor_k, rev_k_metadata)
+    rev_tensor_v = orchestrate_simulate(output_tensor_v, rev_tensor_v, rev_k_metadata)
+
+    rev_tensor_q = rev_tensor_q.reshape(world_size, total_seq_len, hidden_size_q)
+    rev_tensor_k = rev_tensor_k.reshape(world_size, max_cp_degree, total_seq_len, hidden_size_k)
+    rev_tensor_v = rev_tensor_v.reshape(world_size, max_cp_degree, total_seq_len, hidden_size_k)
+    print("rev correct answer done")
 
     (q_tokens_to_dst_per_dispatch, q_seq_to_dst,
      _, kv_dst_global_seq_id) = intermediates
@@ -295,20 +372,35 @@ def test(args):
         kv_dst_global_seq_id, num_send_tokens_kv,
         bytes_q, bytes_k,
     )
+
     qkv_rev_fa2a_metadata = compute_backward_qkv_a2a_layout_metadata(
         qkv_fwd_fa2a_metadata,
     )
 
-    fa2a_q, fa2a_k, fa2a_v = simulate_qkv_a2a_fwd(
+    fa2a_q, fa2a_k, fa2a_v = simulate_qkv_a2a(
         tensor_q, tensor_k, tensor_v,
         qkv_fwd_fa2a_metadata, fwd_q_metadata, fwd_k_metadata,
-        rev_q_metadata.seq_len, rev_k_metadata.seq_len,
-        element_size, hidden_size_q, hidden_size_k
+        rev_q_metadata, rev_k_metadata,
+        element_size, hidden_size_q, hidden_size_k,
+        max_cp_degree, fwd_k_metadata.dst_rank >= 0,
+        is_fwd=True
     )
     torch.testing.assert_close(output_tensor_q, fa2a_q)
     torch.testing.assert_close(output_tensor_k, fa2a_k)
     torch.testing.assert_close(output_tensor_v, fa2a_v)
     print("pass forward send qkv")
+    fa2a_rev_q, fa2a_rev_k, fa2a_rev_v = simulate_qkv_a2a(
+        fa2a_q, fa2a_k, fa2a_v,
+        qkv_rev_fa2a_metadata, rev_q_metadata, rev_k_metadata,
+        fwd_q_metadata, fwd_k_metadata,
+        element_size, hidden_size_q, hidden_size_k,
+        max_cp_degree, fwd_k_metadata.dst_rank >= 0,
+        is_fwd=False
+    )
+    torch.testing.assert_close(rev_tensor_q, fa2a_rev_q)
+    torch.testing.assert_close(rev_tensor_k, fa2a_rev_k.reshape_as(rev_tensor_k))
+    torch.testing.assert_close(rev_tensor_v, fa2a_rev_v.reshape_as(rev_tensor_v))
+    print("pass reverse recv qkv")
 
 
 if __name__ == "__main__":
