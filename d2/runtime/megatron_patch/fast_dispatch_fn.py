update test megatron layer (numerical not correct)

d2/runtime/megatron_patch/fast_dispatch_fn.py
@@ -5,41 +5,49 @@ from d2.runtime.attn_kernels.dispatch import (
 )
 from d2.runtime.fast_alltoall_metadata import FastAlltoAllMetadata
 
+
+# NOTE: we have to add the arg stream here because backward cannot be assigned
+# a stream outside this function.
 class qkv_dispatch(torch.autograd.Function):
     @staticmethod
     def forward(ctx, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor,
-                metadata: FastAlltoAllMetadata, bwd_metadata: FastAlltoAllMetadata):
-        recv_q_shape = metadata.tensor_shape[0].recv_shape
-        recv_k_shape = metadata.tensor_shape[1].recv_shape
-        recv_q = torch.empty(
-            recv_q_shape, dtype=q.dtype, device=q.device)
-        recv_k = torch.empty(
-            recv_k_shape, dtype=k.dtype, device=k.device)
-        recv_v = torch.empty(
-            recv_k_shape, dtype=v.dtype, device=v.device)
+                metadata: FastAlltoAllMetadata, bwd_metadata: FastAlltoAllMetadata,
+                stream: torch.cuda.Stream):
+        if metadata.single_stream:
+            stream = torch.cuda.current_stream()
+        with torch.cuda.stream(stream):
+            recv_q_shape = metadata.tensor_shape[0].recv_shape
+            recv_k_shape = metadata.tensor_shape[1].recv_shape
+            recv_q = torch.empty(
+                recv_q_shape, dtype=q.dtype, device=q.device)
+            recv_k = torch.empty(
+                recv_k_shape, dtype=k.dtype, device=k.device)
+            recv_v = torch.empty(
+                recv_k_shape, dtype=v.dtype, device=v.device)
 
-        fast_a2a_qkv(
-            q, k, v, metadata.kv_replica_mask,
-            recv_q, recv_k, recv_v,
-            # q seq_num_tokens, k seq_num_tokens,
-            metadata.seq_lens[0].send_seqlens, metadata.seq_lens[1].send_seqlens,
-            # q recv_seq_num_tokens, k recv_seq_num_tokens,
-            metadata.seq_lens[0].recv_seqlens, metadata.seq_lens[1].recv_seqlens,
-            # q send_buffer_offset, k send_buffer_offset, v send_buffer_offset,
-            *metadata.send_memcpy_metadata,
-            # q recv_buffer_offset, k recv_buffer_offset, v recv_buffer_offset,
-            *metadata.recv_memcpy_metadata,
-            # sender_send_disp, sender_transfer_sz,
-            # sender_recv_disp, recver_transfer_sz,
-            *metadata.fa2a_metadata,
-            metadata.my_rank_send_offset, metadata.my_rank_recv_offset,
-            metadata.my_rank_send_sz,
-            is_fwd=True,
-            # NOTE: in what case can we guarantee that
-            # the buffer is always already switched?
-            switch_buffer=True,
-        )
+            fast_a2a_qkv(
+                q, k, v, metadata.kv_replica_mask,
+                recv_q, recv_k, recv_v,
+                # q seq_num_tokens, k seq_num_tokens,
+                metadata.seq_lens[0].send_seqlens, metadata.seq_lens[1].send_seqlens,
+                # q recv_seq_num_tokens, k recv_seq_num_tokens,
+                metadata.seq_lens[0].recv_seqlens, metadata.seq_lens[1].recv_seqlens,
+                # q send_buffer_offset, k send_buffer_offset, v send_buffer_offset,
+                *metadata.send_memcpy_metadata,
+                # q recv_buffer_offset, k recv_buffer_offset, v recv_buffer_offset,
+                *metadata.recv_memcpy_metadata,
+                # sender_send_disp, sender_transfer_sz,
+                # sender_recv_disp, recver_transfer_sz,
+                *metadata.fa2a_metadata,
+                metadata.my_rank_send_offset, metadata.my_rank_recv_offset,
+                metadata.my_rank_send_sz,
+                is_fwd=True,
+                # NOTE: in what case can we guarantee that
+                # the buffer is always already switched?
+                switch_buffer=True,
+            )
 
+        ctx.stream = stream
         ctx.q_grad_shape = bwd_metadata.tensor_shape[0].recv_shape
         ctx.k_grad_shape = bwd_metadata.tensor_shape[1].recv_shape
         ctx.grad_my_rank_send_offset = bwd_metadata.my_rank_send_offset
@@ -60,43 +68,48 @@ class qkv_dispatch(torch.autograd.Function):
             *grad_qkv_send_offset, *grad_qkv_recv_offset, *grad_fa2a_metadata
         ]
         ctx.save_for_backward(*grad_metadata)
+        return recv_q, recv_k, recv_v
 
     @staticmethod
     def backward(ctx, grad_recv_q: torch.Tensor, grad_recv_k: torch.Tensor, grad_recv_v: torch.Tensor):
+        stream = ctx.stream
 
-        grad_q_shape = ctx.q_grad_shape
-        grad_k_shape = ctx.k_grad_shape
-        grad_q = torch.empty(grad_q_shape, dtype=grad_recv_q.dtype, device=grad_recv_q.device)
-        grad_k = torch.empty(grad_k_shape, dtype=grad_recv_k.dtype, device=grad_recv_k.device)
-        grad_v = torch.empty_like(grad_recv_v)
-        fast_a2a_qkv(
-            grad_recv_q, grad_recv_k, grad_recv_v, ctx.saved_tensors[0],
-            grad_q, grad_k, grad_v,
-            # q seq_num_tokens, k seq_num_tokens,
-            *ctx.saved_tensors[1:3],
-            # q recv_seq_num_tokens, k recv_seq_num_tokens,
-            *ctx.saved_tensors[3:5],
-            # q send_buffer_offset, k send_buffer_offset, v send_buffer_offset,
-            *ctx.saved_tensors[5:8],
-            # q recv_buffer_offset, k recv_buffer_offset, v recv_buffer_offset,
-            *ctx.saved_tensors[8:11],
-            # sender_send_disp, sender_transfer_sz,
-            # sender_recv_disp, recver_transfer_sz,
-            *ctx.saved_tensors[11:15],
-            ctx.grad_my_rank_send_offset, ctx.grad_my_rank_recv_offset,
-            ctx.grad_my_rank_send_sz,
-            is_fwd=False,
-        )
+        with torch.cuda.stream(stream):
+            grad_q_shape = ctx.q_grad_shape
+            grad_k_shape = ctx.k_grad_shape
+            grad_q = torch.empty(grad_q_shape, dtype=grad_recv_q.dtype, device=grad_recv_q.device)
+            grad_k = torch.empty(grad_k_shape, dtype=grad_recv_k.dtype, device=grad_recv_k.device)
+            grad_v = torch.empty_like(grad_recv_v)
+            fast_a2a_qkv(
+                grad_recv_q, grad_recv_k, grad_recv_v, ctx.saved_tensors[0],
+                grad_q, grad_k, grad_v,
+                # q seq_num_tokens, k seq_num_tokens,
+                *ctx.saved_tensors[1:3],
+                # q recv_seq_num_tokens, k recv_seq_num_tokens,
+                *ctx.saved_tensors[3:5],
+                # q send_buffer_offset, k send_buffer_offset, v send_buffer_offset,
+                *ctx.saved_tensors[5:8],
+                # q recv_buffer_offset, k recv_buffer_offset, v recv_buffer_offset,
+                *ctx.saved_tensors[8:11],
+                # sender_send_disp, sender_transfer_sz,
+                # sender_recv_disp, recver_transfer_sz,
+                *ctx.saved_tensors[11:15],
+                ctx.grad_my_rank_send_offset, ctx.grad_my_rank_recv_offset,
+                ctx.grad_my_rank_send_sz,
+                is_fwd=False,
+            )
+            grad_k = grad_k.sum(dim=0)
+            grad_v = grad_v.sum(dim=0)
 
-        return (grad_q, grad_k, grad_v) + (None,) * 2
+        return (grad_q, grad_k, grad_v) + (None,) * 3
 
 
 class attn_out_dispatch(torch.autograd.Function):
     @staticmethod
     def forward(ctx, attn_out: torch.Tensor,
-                metadata: FastAlltoAllMetadata, bwd_metadata: FastAlltoAllMetadata):
-        stream = metadata.stream
-        if metadata.single_stream:
+                metadata: FastAlltoAllMetadata, bwd_metadata: FastAlltoAllMetadata,
+                stream: torch.cuda.Stream = None):
+        if stream is None or metadata.single_stream:
             stream = torch.cuda.current_stream()
         with torch.cuda.stream(stream):
             recv_shape = metadata.tensor_shape[0].recv_shape
@@ -118,7 +131,6 @@ class attn_out_dispatch(torch.autograd.Function):
                 *metadata.fa2a_metadata,
                 metadata.my_rank_send_offset, metadata.my_rank_recv_offset,
                 metadata.my_rank_send_sz,
-                is_fwd=True,
             )
         ctx.stream = stream
         ctx.rev_recv_shape = bwd_metadata.tensor_shape[0].recv_shape
@@ -160,7 +172,6 @@ class attn_out_dispatch(torch.autograd.Function):
                 *ctx.saved_tensors[4:8],
                 ctx.grad_my_rank_send_offset, ctx.grad_my_rank_recv_offset,
                 ctx.grad_my_rank_send_sz,
-                is_fwd=False,
             )
-        return grad_attn_out + (None,) * 2
+        return (grad_attn_out,) + (None,) * 3
 

d2/runtime/megatron_patch/transformer_layer.py
@@ -24,7 +24,8 @@ from megatron.core.transformer.transformer_layer import (
 from megatron.core.utils import WrappedTensor, make_viewless_tensor
 
 from d2.runtime.megatron_patch.packed_seq_params import PingPangPackedSeqParams, PingPangSingleStepPackedSeqParams
-from d2.runtime.megatron_patch.dispatcher_wrapper import n_to_n_dispatch, TickSync
+from d2.runtime.megatron_patch.dispatcher_wrapper import TickSync
+from d2.runtime.megatron_patch.fast_dispatch_fn import qkv_dispatch, attn_out_dispatch
 
 
 #### Tool functions for splitting and gathering args ####
@@ -83,7 +84,7 @@ class TransformerLayer(MegatronTransformerLayer):
 
     def _layout_attn_to_mlp(
         self, attn_out: torch.Tensor,
-        packed_seq_params: PingPangPackedSeqParams,
+        packed_seq_params: PingPangSingleStepPackedSeqParams,
     ):
         """
         Communication between attention layout and mlp layout.
@@ -101,13 +102,10 @@ class TransformerLayer(MegatronTransformerLayer):
             head_dim = attn_out.shape[-1]
             attn_out = attn_out.view(attn_out.shape[0], num_heads * head_dim)
             ####
-            attn_out_mlp_layout, _ = n_to_n_dispatch.apply(
-                attn_out, # query_in
-                packed_seq_params.attn_to_mlp_metadata, # query_metadata
-                packed_seq_params.mlp_to_attn_metadata, # rev_query_metadata
-                None, # key_value_in
-                None, # key_value_metadata
-                None, # rev_key_value_metadata
+            attn_out_mlp_layout = attn_out_dispatch.apply(
+                attn_out,
+                packed_seq_params.attn_out_fwd_metadata,
+                packed_seq_params.attn_out_bwd_metadata,
                 packed_seq_params.stream, # stream
             )
             #### switch layout back
@@ -118,12 +116,15 @@ class TransformerLayer(MegatronTransformerLayer):
 
     def _layout_mlp_to_attn(
         self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor,
-        packed_seq_params: PingPangPackedSeqParams,
+        packed_seq_params: PingPangSingleStepPackedSeqParams,
     ):
         """
         Communication between attention layout and mlp layout.
         This operation runs on the stream `packed_seq_params.stream`.
         """
+        # Although we set cuda stream in the dispatch function (qkv_dispatch)
+        # since in this function there are some implicit reshape, we add
+        # cuda stream context as well.
         if packed_seq_params.stream is not None:
             context = torch.cuda.stream(packed_seq_params.stream)
         else:
@@ -139,29 +140,23 @@ class TransformerLayer(MegatronTransformerLayer):
             num_kv_heads = key.shape[-2]
             q_head_dim = query.shape[-1]
             kv_head_dim = key.shape[-1]
-            # NOTE: qkv used to be concatenated, so here they are not contiguous, and
-            # the reshape is an extra copy.
+            # qkv used to be concatenated, so here they are not contiguous, the reshape is an extra copy.
+            # TODO: write memcpy to nvshmem buffer kernel with stride support.
             query = query.reshape(query.shape[0], num_q_heads * q_head_dim)
             key = key.reshape(key.shape[0], num_kv_heads * kv_head_dim)
             value = value.reshape(value.shape[0], num_kv_heads * kv_head_dim)
             ####
-
-            key_value = torch.cat([key, value], dim=-1).contiguous()
-            query_attn_layout, key_value_attn_layout = n_to_n_dispatch.apply(
-                query,  # query_in
-                packed_seq_params.mlp_to_attn_metadata, # query_metadata
-                packed_seq_params.attn_to_mlp_metadata, # rev_query_metadata
-                key_value, # key_value_in
-                packed_seq_params.mlp_to_attn_kv_metadata, # key_value_metadata
-                packed_seq_params.mlp_to_attn_kv_grad_metadata, # rev_key_value_metadata
+            query_attn_layout, key_attn_layout, value_attn_layout = qkv_dispatch.apply(
+                query, key, value,
+                packed_seq_params.qkv_fwd_metadata, # query_metadata
+                packed_seq_params.qkv_bwd_metadata, # rev_query_metadata
                 packed_seq_params.stream, # stream
             )
-            key_attn_layout, value_attn_layout = key_value_attn_layout.split(key_value_attn_layout.shape[1] // 2, dim=1)
 
             #### switch layout back
             query_attn_layout = query_attn_layout.view(-1, num_q_heads, q_head_dim).contiguous()
-            key_attn_layout = key_attn_layout.reshape(-1, num_kv_heads, kv_head_dim).contiguous()
-            value_attn_layout = value_attn_layout.reshape(-1, num_kv_heads, kv_head_dim).contiguous()
+            key_attn_layout = key_attn_layout.view(-1, num_kv_heads, kv_head_dim).contiguous()
+            value_attn_layout = value_attn_layout.view(-1, num_kv_heads, kv_head_dim).contiguous()
             torch.cuda.nvtx.range_pop()
 
         return query_attn_layout, key_attn_layout, value_attn_layout
@@ -335,7 +330,7 @@ class TransformerLayer(MegatronTransformerLayer):
         rotary_pos_emb: Optional[Tensor] = None,
         rotary_pos_cos: Optional[Tensor] = None,
         rotary_pos_sin: Optional[Tensor] = None,
-        packed_seq_params: Optional[PingPangPackedSeqParams] = None,
+        packed_seq_params: Optional[PingPangSingleStepPackedSeqParams] = None,
         sequence_len_offset: Optional[Tensor] = None,
     ):
         """
@@ -422,7 +417,7 @@ class TransformerLayer(MegatronTransformerLayer):
         attention_mask: Optional[Tensor] = None,
         attention_bias: Optional[Tensor] = None,
         attn_mask_type: Optional[AttnMaskType] = None,
-        packed_seq_params: Optional[PingPangPackedSeqParams] = None,
+        packed_seq_params: Optional[PingPangSingleStepPackedSeqParams] = None,
     ):
         """
         Copied from megatron.core.transformer.attention.Attention.forward

playground/deprecated/test_megatron_layer.py
@@ -18,9 +18,8 @@ from d2.runtime.megatron_patch.model_patch import get_gpt_layer_with_transformer
 from d2.runtime.megatron_patch.packed_seq_params import PingPangPackedSeqParams, PingPangSingleStepPackedSeqParams
 from d2.runtime.megatron_patch.transformer_layer import TransformerLayer as PingPangTransformerLayer
 
-from test_comm_metadata import orchestrate_simulate
 from deprecated.test_dispatch_qkv import create_testcase_qkv
-from test_util import MegatronBaseWorker, ParallelConfig
+from test_util import MegatronBaseWorker, ParallelConfig, simulate_communication
 
 
 def create_pg(num_nodes: int, num_gpus_per_node: int, worker_cls, nsys_profile:bool = False):
@@ -127,34 +126,6 @@ def get_seqlen_shard(cu_seqlens_q: torch.Tensor, cu_seqlens_kv: torch.Tensor,
         cu_seqlens_q = prepend_zero_fn(cu_seqlens_q)
         cu_seqlens_kv = prepend_zero_fn(cu_seqlens_kv)
     return cu_seqlens_q, cu_seqlens_kv, max_seqlen_q, max_seqlen_kv, num_seq
-
-
-def simulate_communication(tensors: list[torch.Tensor], metadata: Metadata):
-    world_size = len(tensors)
-    assert world_size == metadata.world_size
-    output_seq_len = metadata.num_recv_tokens.max()
-    input_pad_len = max(tensor.shape[0] for tensor in tensors)
-    pad_tensors = [
-        torch.cat([
-            tensor,
-            torch.zeros(
-                (input_pad_len - tensor.shape[0], *tensor.shape[1:]),
-                dtype=tensor.dtype, device=tensor.device
-            )
-        ], dim=0).unsqueeze(0) for tensor in tensors
-    ]
-    input_tensor = torch.cat(pad_tensors, dim=0)
-    output_tensor = torch.zeros((world_size, output_seq_len, *input_tensor.shape[2:]), dtype=input_tensor.dtype, device=input_tensor.device)
-    output_tensor = orchestrate_simulate(
-        input_tensor.reshape(world_size, input_pad_len, -1),
-        output_tensor.reshape(world_size, output_seq_len, -1),
-        metadata
-    ).reshape(world_size, output_seq_len, *input_tensor.shape[2:])
-    output_tensors = torch.split(output_tensor, 1, dim=0)
-    output_tensors_split = [
-        t[0, :metadata.num_recv_tokens[rank].max()] for rank, t in enumerate(output_tensors)
-    ]
-    return output_tensors_split
 
 
 @torch.no_grad()