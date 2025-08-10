dummy backward (pp runnable, numerical wrong

d2/runtime/megatron_patch/forward_backward_func.py
@@ -100,6 +100,7 @@ def forward_backward_pipelining_without_interleaving(
     forward_only: bool = False,
     collect_non_loss_data: bool = False,
     first_val_step: bool = None,
+    dummy_bwd_func=None,
 ):
     """Run non-interleaved 1F1B schedule, with communication between pipeline
     stages. Returns dictionary with losses if the last stage, empty dict otherwise."""
@@ -204,6 +205,7 @@ def forward_backward_pipelining_without_interleaving(
         input_tensors = []
         output_tensors = []
     forward_data_store = []
+    dummy_data_store = []
 
     global forward_backward_pipelining_without_interleaving_first_run
     if forward_backward_pipelining_without_interleaving_first_run:
@@ -221,20 +223,21 @@ def forward_backward_pipelining_without_interleaving(
             # torch.distributed.barrier()
             print("=====", flush=True)
             # torch.distributed.barrier()
-            _ = forward_step(
-                forward_step_func,
-                data_iterator,
-                model,
-                num_microbatches,
-                [torch.tensor([]).cuda()],
-                forward_data_store,
-                config,
-                collect_non_loss_data,
-                None,
-                False,
-                current_microbatch=i - rank,
-                encoder_decoder_xattn=encoder_decoder_xattn,
-            )
+            with torch.no_grad():
+                _ = forward_step(
+                    forward_step_func,
+                    data_iterator,
+                    model,
+                    num_microbatches,
+                    [torch.tensor([]).cuda()],
+                    dummy_data_store,
+                    config,
+                    collect_non_loss_data,
+                    None,
+                    False,
+                    current_microbatch=i - rank,
+                    encoder_decoder_xattn=encoder_decoder_xattn,
+                )
         else:
             # Decide to checkpoint all layers' activations of the current micro-batch
             if max_outstanding_backprops is not None:
@@ -272,6 +275,7 @@ def forward_backward_pipelining_without_interleaving(
             if not forward_only:
                 input_tensors.append(input_tensor)
                 output_tensors.append(output_tensor)
+            print(f"warmup !!!{rank=}, {i=}, {input_tensor=}, {output_tensor=}")
 
             print(f"send recv forward {rank=}, {i=}")
             input_tensor = send_forward_recv_forward(
@@ -297,33 +301,30 @@ def forward_backward_pipelining_without_interleaving(
         next_forward_dummy = i + 1 >= num_microbatches_remaining
         next_backward_dummy = i + 1 < num_warmup_microbatches
         if i == 0 == num_warmup_microbatches:
-            print(f"recv backward {rank=}, {i=}")
             output_tensor_grad = recv_backward(send_tensor_shapes, config)
-            print(f"finish recv backward {rank=}, {i=}")
         if i == 0 == num_warmup_microbatches:
-            print(f"recv forward {rank=}, {i=}")
             input_tensor = recv_forward(recv_tensor_shapes, config)
-            print(f"finish recv forward {rank=}, {i=}")
 
         if forward_dummy:
             print(f'forward dummy {rank=}, {i=}, {num_warmup_microbatches=}')
             # torch.distributed.barrier()
             print("=====", flush=True)
             # torch.distributed.barrier()
-            _ = forward_step(
-                forward_step_func,
-                data_iterator,
-                model,
-                num_microbatches,
-                [torch.tensor([]).cuda()],
-                forward_data_store,
-                config,
-                collect_non_loss_data,
-                None,
-                False,
-                current_microbatch=i - num_microbatches_remaining - parallel_state.get_pipeline_model_parallel_world_size(),
-                encoder_decoder_xattn=encoder_decoder_xattn,
-            )
+            with torch.no_grad():
+                _ = forward_step(
+                    forward_step_func,
+                    data_iterator,
+                    model,
+                    num_microbatches,
+                    [torch.tensor([]).cuda()],
+                    dummy_data_store,
+                    config,
+                    collect_non_loss_data,
+                    None,
+                    False,
+                    current_microbatch=i - num_microbatches_remaining - parallel_state.get_pipeline_model_parallel_world_size(),
+                    encoder_decoder_xattn=encoder_decoder_xattn,
+                )
         else:
             print(f'forward {rank=}, {i=}, {num_warmup_microbatches=}')
             # torch.distributed.barrier()
@@ -357,6 +358,8 @@ def forward_backward_pipelining_without_interleaving(
             )
             total_num_tokens += num_tokens
 
+            print(f'normal forward !!!{rank=}, {i=}, {input_tensor=}, {output_tensor=}')
+
             if forward_only:
                 if parallel_state.is_pipeline_last_stage():
                     output_tensor = [None]
@@ -386,12 +389,10 @@ def forward_backward_pipelining_without_interleaving(
                 # torch.distributed.barrier()
                 # get some dummy tensors for now
                 # FIXME: implement a truly dummy backward
-                input_tensor = input_tensors[0]
-                output_tensor = output_tensors[0]
-                output_tensor_grad = [torch.zeros_like(t) for t in output_tensor]
-                _ = backward_step(
-                    input_tensor, output_tensor, output_tensor_grad, model_type, config, retain_graph=True
-                )
+                # input_tensor = input_tensors[0]
+                # output_tensor = output_tensors[0]
+                # output_tensor_grad = [torch.zeros_like(t) for t in output_tensor]
+                dummy_bwd_func(model)
 
             else:
                 print(f'backward {send_tensor_shapes=}, {rank=}, {i=}, {num_microbatches_remaining=}')
@@ -454,6 +455,7 @@ def forward_backward_pipelining_without_interleaving(
                 # torch.distributed.barrier()
                 print("=====", flush=True)
                 # torch.distributed.barrier()
+                dummy_bwd_func(model)
             else:
                 print(f'backward {send_tensor_shapes=}, {rank=}, {i=}, {num_microbatches_remaining=}')
                 # output_tensor_grad = recv_backward(send_tensor_shapes, config)

d2/runtime/megatron_patch/fused_comm_attn.py
@@ -3,10 +3,12 @@ import math
 from typing import Tuple
 
 from flash_attn.flash_attn_interface import (
-    _wrapped_flash_attn_varlen_forward, _wrapped_flash_attn_varlen_backward,
-    flash_attn_varlen_func
+    _wrapped_flash_attn_varlen_forward, _wrapped_flash_attn_varlen_backward
 )
+from d2.runtime.attn_kernels.ops import fast_a2a
+from d2.runtime.megatron_patch.packed_seq_params import PingPangSingleStepPackedSeqParams
 from megatron.core.packed_seq_params import PackedSeqParams
+from megatron.core.transformer.transformer_config import TransformerConfig
 import torch
 from torch import Tensor
 
@@ -318,3 +320,113 @@ class post_a2a_attn_out_with_lse(torch.autograd.Function):
 
         signal_grad = grad_attn_out.new_zeros((1,))
         return signal_grad, *((None,) * 7)
+
+
+@torch.no_grad()
+def dummy_backward(
+    config: TransformerConfig,
+    packed_seq_params: PingPangSingleStepPackedSeqParams,
+    dtype: torch.dtype,
+    device: torch.device,
+):
+    assert packed_seq_params.bwd_packed_seq_params is not None
+
+    # create some dummy data
+    q_len = packed_seq_params.qkv_fwd_metadata.seq_lens[0].send_seqlens.sum().item()
+    kv_len = packed_seq_params.qkv_fwd_metadata.seq_lens[1].send_seqlens.sum().item()
+    attn_out = torch.zeros((q_len, config.hidden_size), dtype=dtype, device=device)
+    grad_attn_out = torch.zeros_like(attn_out)
+    num_heads = config.num_attention_heads // config.tensor_model_parallel_size
+    softmax_lse = torch.zeros((q_len, num_heads), dtype=torch.float32, device=device)
+    q = torch.zeros_like(attn_out)
+    k = torch.zeros((kv_len, config.hidden_size), dtype=dtype, device=device)
+    v = torch.zeros_like(k)
+
+    dispatcher_id = packed_seq_params.dispatcher_id
+
+    bwd_attn_out_qkv_metadata = packed_seq_params.attn_out_bwd_metadata
+    bwd_qkv_metadata = packed_seq_params.qkv_bwd_metadata
+    bwd_fa_params = packed_seq_params.bwd_packed_seq_params
+
+    pre_fast_a2a_attn_out_grad_resend_qkv(
+        grad_attn_out, attn_out, softmax_lse, q, k, v,
+        bwd_attn_out_qkv_metadata.kv_replica_mask,
+        bwd_attn_out_qkv_metadata.seq_lens[0].send_seqlens,
+        bwd_attn_out_qkv_metadata.seq_lens[1].send_seqlens,
+        *bwd_attn_out_qkv_metadata.send_memcpy_metadata,
+        instance_id=dispatcher_id,
+    )
+
+    with torch.cuda.stream(packed_seq_params.stream):
+        fast_a2a(
+            *bwd_attn_out_qkv_metadata.fa2a_metadata,
+            bwd_attn_out_qkv_metadata.my_rank_send_offset,
+            bwd_attn_out_qkv_metadata.my_rank_recv_offset,
+            bwd_attn_out_qkv_metadata.my_rank_send_sz,
+            instance_id=dispatcher_id,
+        )
+    
+    recv_k_shape = bwd_attn_out_qkv_metadata.tensor_shape[1].recv_shape
+    recv_k = torch.empty(recv_k_shape, dtype=dtype, device=device)
+    recv_v = torch.empty_like(recv_k)
+    recv_q_len = bwd_attn_out_qkv_metadata.seq_lens[0].recv_seqlens.sum().item()
+    recv_q_shape = recv_q_len, config.hidden_size // config.tensor_model_parallel_size
+    softmax_lse_shape = recv_q_len, num_heads
+    (recv_attn_out_grad, recv_attn_out, recv_lse, recv_q, recv_k, recv_v
+    ) = post_fast_a2a_attn_out_grad_resend_qkv(
+        recv_q_shape, softmax_lse_shape, recv_q_shape, torch.float32,
+        recv_k, recv_v,
+        None,
+        bwd_attn_out_qkv_metadata.seq_lens[0].recv_seqlens,
+        bwd_attn_out_qkv_metadata.seq_lens[1].recv_seqlens,
+        *bwd_attn_out_qkv_metadata.recv_memcpy_metadata,
+        is_fwd=True,
+        switch_buffer=dispatcher_id is None,
+        instance_id=dispatcher_id,
+    )
+
+    recv_lse = recv_lse.T.contiguous()
+    dq, dk, dv = _qkv_to_attn_out_bwd(
+        recv_q, recv_k, recv_v, recv_attn_out, recv_attn_out_grad,
+        recv_lse,
+        FlashAttnArgs(
+            num_heads_q=config.num_attention_heads // config.tensor_model_parallel_size,
+            num_heads_kv=config.num_query_groups // config.tensor_model_parallel_size,
+            head_dim=config.hidden_size // config.num_attention_heads,
+            return_attn_probs=True,
+        ),
+        bwd_fa_params.cu_seqlens_q, bwd_fa_params.cu_seqlens_kv,
+        bwd_fa_params.max_seqlen_q, bwd_fa_params.max_seqlen_kv,
+    )
+
+    pre_fast_a2a_qkv(
+        dq, dk, dv, None,
+        bwd_qkv_metadata.seq_lens[0].send_seqlens,
+        bwd_qkv_metadata.seq_lens[1].send_seqlens,
+        *bwd_qkv_metadata.send_memcpy_metadata,
+        is_fwd=False,
+        instance_id=dispatcher_id,
+    )
+
+    with torch.cuda.stream(packed_seq_params.stream):
+        fast_a2a(
+            *bwd_qkv_metadata.fa2a_metadata,
+            bwd_qkv_metadata.my_rank_send_offset,
+            bwd_qkv_metadata.my_rank_recv_offset,
+            bwd_qkv_metadata.my_rank_send_sz,
+            instance_id=dispatcher_id,
+        )
+    
+    grad_q = q.new_empty(bwd_qkv_metadata.tensor_shape[0].recv_shape)
+    grad_k = k.new_empty(bwd_qkv_metadata.tensor_shape[1].recv_shape)
+    grad_v = torch.empty_like(grad_k)
+    post_fast_a2a_qkv(
+        grad_q, grad_k, grad_v,
+        bwd_qkv_metadata.kv_replica_mask,
+        bwd_qkv_metadata.seq_lens[0].recv_seqlens,
+        bwd_qkv_metadata.seq_lens[1].recv_seqlens,
+        *bwd_qkv_metadata.recv_memcpy_metadata,
+        is_fwd=False,
+        switch_buffer=dispatcher_id is None,
+        instance_id=dispatcher_id,
+    )

d2/runtime/megatron_patch/transformer_layer.py
@@ -1,4 +1,5 @@
 from contextlib import nullcontext
+import dis
 import functools
 from typing import Any, Dict, List, Optional, Union
 import types
@@ -7,6 +8,9 @@ import warnings
 import torch
 from torch import Tensor
 
+from d2.runtime.attn_kernels import dispatch
+from d2.runtime.attn_kernels.dispatch import post_fast_a2a_attn_out_grad_resend_qkv, pre_fast_a2a_attn_out_grad_resend_qkv
+from d2.runtime.attn_kernels.ops import fast_a2a
 from megatron.core import tensor_parallel, parallel_state
 from megatron.core.inference.contexts import BaseInferenceContext
 from megatron.core.models.gpt.gpt_model import GPTModel
@@ -15,7 +19,7 @@ from megatron.core.transformer.transformer_block import (
 )
 from megatron.core.utils import WrappedTensor, make_viewless_tensor
 
-from d2.runtime.megatron_patch.fused_comm_attn import FlashAttnArgs, FusedCommAttn, post_a2a_attn_out_with_lse
+from d2.runtime.megatron_patch.fused_comm_attn import FlashAttnArgs, FusedCommAttn, dummy_backward, post_a2a_attn_out_with_lse
 from d2.runtime.megatron_patch.base_transformer_layer import TransformerLayer as BaseTransformerLayer
 from d2.runtime.megatron_patch.packed_seq_params import PingPangPackedSeqParams, PingPangSingleStepPackedSeqParams
 from d2.runtime.megatron_patch.stream_sync_fn import TickSync
@@ -820,6 +824,12 @@ class PingPangGPTModel(GPTModel):
         if decoder_pre_process_flag:
             self.decoder.pre_process = False
         return output
+
+    def dummy_backward(self, packed_seq_params: PingPangPackedSeqParams):
+        dtype = self.decoder.layers[0].self_attention.linear_qkv.weight.dtype
+        device = self.decoder.layers[0].self_attention.linear_qkv.weight.device
+        for _ in self.decoder.layers:
+            dummy_backward(self.config, packed_seq_params, dtype, device)
 
     def init_ping_pong_communication_ctx(self, device: torch.device):
         self.decoder.init_ping_pang_communication_ctx(device)

tests/test_megatron_e2e_pipeline.py
@@ -170,7 +170,7 @@ class MegatronE2eWorker(MegatronBaseWorker):
         dummy_microbatch = microbatches[0]  # FIXME: this is important for all-to-all
 
         def wrap_iter(batch_iter):
-            if mpu.is_pipeline_first_stage() or mpu.is_pipeline_last_stage():
+            if False:  #mpu.is_pipeline_first_stage() or mpu.is_pipeline_last_stage():
                 # hardcode here: pipeline parallel add this number of dummy forwards
                 for _ in range(mpu.get_data_parallel_rank()):
                     print('yield pre dummy')
@@ -193,9 +193,12 @@ class MegatronE2eWorker(MegatronBaseWorker):
             # returns "hidden_states" if not model.post_process (not the last layer)
             # returns "logits" when label is None.
             output = gptmodel_forward(
-                model, input_ids, attention_mask, position_ids, self.tf_config.sequence_parallel, packed_seq_params
+                model, input_ids, attention_mask, position_ids, self.tf_config.sequence_parallel, packed_seq_params, labels=input_ids.unsqueeze(0),
             )
             return output, loss_func
+
+        def dummy_backward_step(model):
+            unwrap_model(model).dummy_backward(dummy_microbatch['packed_seq_params'])
 
         batch_generator = make_batch_generator(microbatches, vpp_size=len(self.train_module))
         batch_generator = wrap_iter(batch_generator)
@@ -208,6 +211,7 @@ class MegatronE2eWorker(MegatronBaseWorker):
                 seq_length=total_seqlen,  # no use, since variable_seq_lengths=True
                 micro_batch_size=1,  # no use when input_shapes was set
                 forward_only=forward_only,
+                dummy_bwd_func=dummy_backward_step,
             )
         else:
             losses_reduced = forward_backward_func(
@@ -219,6 +223,7 @@ class MegatronE2eWorker(MegatronBaseWorker):
                 micro_batch_size=1,  # in use for pp = 1
                 forward_only=forward_only,
             )
+        print(f'{losses_reduced=}')
         return losses_reduced
 
     def _build_model_optimizer(self,
@@ -359,10 +364,13 @@ def test(args):
     )
 
     set_random_seed(seed, set_megatron=False)
-    input_ids = torch.randint(0, 100, (as_world_size, total_seq_len))
-    position_ids = torch.arange(total_seq_len).repeat(as_world_size, 1)
-    input_ids_local = input_ids[as_rank]
-    position_ids_local = position_ids[as_rank]
+    # input_ids = torch.randint(0, 100, (as_world_size, total_seq_len))
+    # position_ids = torch.arange(total_seq_len).repeat(as_world_size, 1)
+    # input_ids_local = input_ids[as_rank]
+    # position_ids_local = position_ids[as_rank]
+    # print(input_ids_local.shape, position_ids_local.shape)
+    input_ids_local = torch.randint(0, 100, (total_seq_len,))
+    position_ids_local = torch.arange(total_seq_len)
     (cu_seqlens_q, cu_seqlens_kv, max_seqlen_q, max_seqlen_kv, *_) = fa_bwd_params
     bwd_packed_seq_params = PackedSeqParams(
         cu_seqlens_q=cu_seqlens_q[rank],
@@ -430,8 +438,8 @@ if __name__ == "__main__":
     parser.add_argument("--num-seqs", type=int, default=3)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--num-nodes", type=int, default=1)
-    parser.add_argument("--num-gpus-per-node", type=int, default=2)
+    parser.add_argument("--num-gpus-per-node", type=int, default=4)
     parser.add_argument("--tp-size", type=int, default=1)
-    parser.add_argument("--pp-size", type=int, default=2)
+    parser.add_argument("--pp-size", type=int, default=4)
     args = parser.parse_args()
     test(args)