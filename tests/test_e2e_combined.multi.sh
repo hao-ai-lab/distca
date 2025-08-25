48f76715eb1192364a55509eaa523cb71a4a2e31
GindaChen <32371474+GindaChen@users.noreply.github.com>
2025-08-25 18:17:51 +0000

    Make things log into the `logs` files.

--- tests/test_e2e_combined.multi.sh
+++ tests/test_e2e_combined.multi.sh
@@ -14,7 +14,7 @@ WLBLLM_SYNC_TIME_AG=${WLBLLM_SYNC_TIME_AG:-0}
 EXPERIMENT_REPEAT_TIMES=${EXPERIMENT_REPEAT_TIMES:-3}
 EXPERIMENT_WARMUP_TIMES=${EXPERIMENT_REPEAT_TIMES:-5}
 EXPERIMENT_NVSHMEM_BUFFER_SIZE_GB=${EXPERIMENT_NVSHMEM_BUFFER_SIZE_GB:--1}
-
+EXPERIMENT_SHOULD_FORCE_EXIT=${EXPERIMENT_SHOULD_FORCE_EXIT:-0}
 
 export NVTE_NVTX_ENABLED=1
 export NSYS_NVTX_PROFILER_REGISTER_ONLY=0 
@@ -47,13 +47,6 @@ if [ -z "${RZV_ENDPOINT}" ]; then
 fi
 
 
-echo
-echo "Executing: bash $0 rzv_endpoint=$1 n_nodes=$2 n_gpus_per_node=$3 tp_size=$4 mode=$5 rzv_id=$6"
-echo 
-echo "bash $0 $1 $2 $3 $4 $5 $6"
-echo
-
-
 D2_FA2A_DISABLE_SEND_RECV=${D2_FA2A_DISABLE_SEND_RECV:-0}
 
 CP_DEGREE=${CP_DEGREE:-1}
@@ -72,38 +65,48 @@ SHOULD_ADD_DEBUG_CASES=${SHOULD_ADD_DEBUG_CASES:-0}
 
 THIS_HOST=$(hostname)
 
-OUTPUT_DIR=${OUTPUT_DIR:-nsys-profile}
+CURDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DEFAULT_OUTPUT_DIR=${DEFAULT_OUTPUT_DIR:-${CURDIR}/../logs}
+OUTPUT_DIR=${OUTPUT_DIR:-${DEFAULT_OUTPUT_DIR}}
+OUTPUT_DIR=$(realpath ${OUTPUT_DIR})
+
+
 now="$(TZ='America/Los_Angeles' date +%Y%m%d_%H%M%S)_PST"
 mkdir -p ${OUTPUT_DIR}
 output_file_stem=${OUTPUT_DIR}/${now}.${MODE}.${THIS_HOST}.nnodes${NNODES}.bs${BATCH_SIZE}.maxid${MAX_SAMPLE_ID}.tp${TP_SIZE}.cp${CP_DEGREE}.t${NUM_TOKENS}.elong${ELONGATE_FACTOR}.up${UP_SAMPLE_FACTOR}.ft${FILTER_THRESHOLD}.fr${FILTER_RATIO}
 NSYS_PROFILE_PATH=${output_file_stem}.nsys-rep
 LOG_PATH=${output_file_stem}.log
 
-echo "Running with the following parameters:"
-echo "  MODE=${MODE}"
-echo "  BATCH_SIZE=${BATCH_SIZE}"
-echo "  TP_SIZE=${TP_SIZE}"
-echo "  CP_DEGREE=${CP_DEGREE}"
-echo "  REPLAN_ITER=${REPLAN_ITER}"
-echo "  NUM_TOKENS=${NUM_TOKENS}"
-echo "  NUM_LAYERS=${NUM_LAYERS}"
-echo "  UP_SAMPLE_FACTOR=${UP_SAMPLE_FACTOR}"
-echo "  ELONGATE_FACTOR=${ELONGATE_FACTOR}"
-echo "  FILTER_THRESHOLD=${FILTER_THRESHOLD}"
-echo "  FILTER_RATIO=${FILTER_RATIO}"
-echo "  MAX_SAMPLE_ID=${MAX_SAMPLE_ID}"
-echo "  MODEL_PATH=${MODEL_PATH}"
-echo "  NNODES=${NNODES}"
-echo "  NPROC_PER_NODE=${NPROC_PER_NODE}"
-echo "  ENABLE_NSYS=${ENABLE_NSYS}"
-echo "  NSYS_PROFILE_PATH=${NSYS_PROFILE_PATH}"
-echo "  SHOULD_ADD_DEBUG_CASES=${SHOULD_ADD_DEBUG_CASES}"
-echo "  WLBLLM_SYNC_TIME_AG=${WLBLLM_SYNC_TIME_AG}"
-echo "  EXPERIMENT_REPEAT_TIMES=${EXPERIMENT_REPEAT_TIMES}"
-echo "  EXPERIMENT_NVSHMEM_BUFFER_SIZE_GB=${EXPERIMENT_NVSHMEM_BUFFER_SIZE_GB}"
-echo "  EXPERIMENT_WARMUP_TIMES=${EXPERIMENT_WARMUP_TIMES}"
-echo "  OUTPUT_DIR=${OUTPUT_DIR}"
-echo "  LOG_PATH=${LOG_PATH}"
+
+function echo_and_tee() {
+    echo "$@" | tee -a ${LOG_PATH}
+}
+
+echo_and_tee "Running with the following parameters:" 
+echo_and_tee "  MODE=${MODE}"
+echo_and_tee "  BATCH_SIZE=${BATCH_SIZE}"
+echo_and_tee "  TP_SIZE=${TP_SIZE}"
+echo_and_tee "  CP_DEGREE=${CP_DEGREE}"
+echo_and_tee "  REPLAN_ITER=${REPLAN_ITER}"
+echo_and_tee "  NUM_TOKENS=${NUM_TOKENS}"
+echo_and_tee "  NUM_LAYERS=${NUM_LAYERS}"
+echo_and_tee "  UP_SAMPLE_FACTOR=${UP_SAMPLE_FACTOR}"
+echo_and_tee "  ELONGATE_FACTOR=${ELONGATE_FACTOR}"
+echo_and_tee "  FILTER_THRESHOLD=${FILTER_THRESHOLD}"
+echo_and_tee "  FILTER_RATIO=${FILTER_RATIO}"
+echo_and_tee "  MAX_SAMPLE_ID=${MAX_SAMPLE_ID}"
+echo_and_tee "  MODEL_PATH=${MODEL_PATH}"
+echo_and_tee "  NNODES=${NNODES}"
+echo_and_tee "  NPROC_PER_NODE=${NPROC_PER_NODE}"
+echo_and_tee "  ENABLE_NSYS=${ENABLE_NSYS}"
+echo_and_tee "  NSYS_PROFILE_PATH=${NSYS_PROFILE_PATH}"
+echo_and_tee "  SHOULD_ADD_DEBUG_CASES=${SHOULD_ADD_DEBUG_CASES}"
+echo_and_tee "  WLBLLM_SYNC_TIME_AG=${WLBLLM_SYNC_TIME_AG}"
+echo_and_tee "  EXPERIMENT_REPEAT_TIMES=${EXPERIMENT_REPEAT_TIMES}"
+echo_and_tee "  EXPERIMENT_NVSHMEM_BUFFER_SIZE_GB=${EXPERIMENT_NVSHMEM_BUFFER_SIZE_GB}"
+echo_and_tee "  EXPERIMENT_WARMUP_TIMES=${EXPERIMENT_WARMUP_TIMES}"
+echo_and_tee "  OUTPUT_DIR=${OUTPUT_DIR}"
+echo_and_tee "  LOG_PATH=${LOG_PATH}"
 
 # Prepare the common torchrun command and arguments
 TORCHRUN_CMD=(
@@ -136,12 +139,20 @@ if [ ${SHOULD_ADD_DEBUG_CASES} -eq 1 ]; then
     TORCHRUN_CMD+=(--should-add-debug-cases)
 fi
 
-if [ ${SHOULD_FORCE_EXIT} -eq 1 ]; then
+if [ ${EXPERIMENT_SHOULD_FORCE_EXIT} -eq 1 ]; then
     TORCHRUN_CMD+=(--force-exit)
 fi
 
-set -x
+
 if [ ${ENABLE_NSYS} -eq 1 ]; then
+    echo_and_tee nsys profile \
+      --show-output=true \
+      --force-overwrite=true \
+      -o ${NSYS_PROFILE_PATH} \
+      --sample=none \
+      -t cuda,nvtx \
+    torchrun "${TORCHRUN_CMD[@]}"
+
     nsys profile \
       --show-output=true \
       --force-overwrite=true \
@@ -150,6 +161,8 @@ if [ ${ENABLE_NSYS} -eq 1 ]; then
       -t cuda,nvtx \
     torchrun "${TORCHRUN_CMD[@]}" | tee ${LOG_PATH} 2>&1
 else
+    echo_and_tee torchrun "${TORCHRUN_CMD[@]}"
+
+
     torchrun "${TORCHRUN_CMD[@]}" | tee ${LOG_PATH} 2>&1
 fi
-set +x
