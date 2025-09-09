23d3454609f1dde46ab58122c88c969c5d3aa224
GindaChen <32371474+GindaChen@users.noreply.github.com>
2025-09-09 17:05:17 +0000

    add monitor script

--- benchmarks/_250907_large_scale_v6/wandb_monitor.py
+++ benchmarks/_250907_large_scale_v6/wandb_monitor.py
@@ -53,17 +53,49 @@ while True:
         print(f"🟡 {name} has {len(this_benchmark_data)} samples")
 
     df = pd.DataFrame(benchmark_data, columns=['name', 'sample_id', 'duration_ms', 'mode', 'cp_size', 'nodes', 'batch_size', 'num_tokens'])
+    df = df.drop_duplicates(subset=['sample_id', 'name'], keep='last')
+    
     
     # Pivot the dataframe to have sample_id as rows and name as columns
-    pivot_df = df.drop_duplicates(subset=['sample_id', 'name'], keep='last').pivot(index='sample_id', columns='name', values='duration_ms').round(2)
+    pivot_df = df.pivot(index='sample_id', columns='name', values='duration_ms').round(2)
     pivot_df.to_csv(f"pivot_df.csv")
     print(pivot_df)
 
     # Speedup dataframe
     speedup_df = df[[
-        'nodes', 'batch_size', 'num_tokens', 'mode', 'cp_size', 'duration_ms',
+        'nodes', 'batch_size', 'num_tokens', 'mode', 'cp_size', 'sample_id', 'duration_ms',
     ]].sort_values(by=['nodes', 'batch_size', 'num_tokens', 'mode', 'cp_size'], ascending=True)
     speedup_df.to_csv(f"speedup_df.tsv", sep="\t", index=False)
+
+    # Pivot speedup table such that fore each sample_id we have a column, and each row is ('nodes', 'batch_size', 'num_tokens', 'mode', 'cp_size')
+    pivot_speedup_df = speedup_df.pivot(index=['nodes', 'batch_size', 'num_tokens', 'mode', 'cp_size'], columns='sample_id', values='duration_ms').round(2)
+    pivot_speedup_df.to_csv(f"pivot_speedup_df.tsv", sep="\t", index=True)
+    print(pivot_speedup_df)
+    # pivot_speedup_df['speedup'] = 0
+    speedups = []
+    for row in pivot_speedup_df.iterrows():
+        node, bs, t, mode, cp = row[0]
+        if mode == 'wlbllm':
+            try:
+                # breakpoint()
+                d2_avg_time = speedup_df[
+                    (speedup_df['node'] == node) &
+                    (speedup_df['batch_size'] == bs) &
+                    (speedup_df['num_tokens'] == t) &
+                    (speedup_df['mode'] == 'd2')
+                ].mean()
+                wlbllm_avg_time = row[1].mean()
+                speedup = wlbllm_avg_time / d2_avg_time
+                speedups.append(speedup)
+            except:
+                speedups.append(0)
+            pass
+        else:
+            speedups.append(1)
+
+    # breakpoint()
+    pivot_speedup_df['speedup'] = speedups
+    pivot_speedup_df.to_csv(f"pivot_speedup_df_with_speedup.tsv", sep="\t", index=True)
 
 
 
